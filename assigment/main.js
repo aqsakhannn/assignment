4ffe08e01bbc628f351d30576537024d05d2a56b
aqsakhannn <159409677+aqsakhannn@users.noreply.github.com>
2024-03-08 04:37:43 -0700

    assignment typescript type and array

---
+++ assigment/main.js
@@ -0,0 +1,56 @@
+//console.log(message);
+//message="123"
+//console.log(message);
+//let personName : string = 'Ahmed';
+//console.log("Good Day, "+ personName+"!")
+//let personAge : number = 25;
+//console.log("persona's age is " + personAge)
+//premetive data type
+//string,number => limited,boolean => true/false,any => string number boolean ,null,undefined,symbol,bigint => unlimited numbers
+//Data structure/Non-premetive data type
+//OBject,array,Function
+//boolean
+//let isvote : boolean = true;
+//let isnotvote : boolean = false;
+//console.log(isvote)
+//console.log(isnotvote);
+//null
+//let personName : null = ;
+//console.log(personName)
+//Date 5th.march.2024
+//3types of case
+//camelCase
+//snake_case
+//PascalCase
+/*let userName : string ='Aqsa' // e.g.camelCase
+let user_name : string ='Hurram' //e.g.snake_case
+let UserName :string = 'Imran Khan' //e.g.pascalCase
+// = is  assignment operator
+let isUserExist : boolean = true ;
+let is_user_exist : boolean = false;
+let IsUserExist : number= 1;
+console.log(userName,user_name,UserName)
+console.log(isUserExist,is_user_exist,IsUserExist)
+// any data type
+let anyType: number;
+anyType = 20;
+console.log(anyType);*/
+//Arrays => [] => 0,..
+//let students : any[]=['Sr Noman','Sr Zia khan','Sr Qasim',1,true,10]
+//let students : string[]=['Sr Noman','Sr Zia khan','Sr Qasim']
+//console.log(students)
+//let firstStudentName =students[1]
+//let lastStudentName =students[2]
+//console.log(lastStudentName)
+//console.log(students[5])
+//console.log(students[1],students[2])
+//TASK
+//creat boolean array
+//creat number array
+var studentsMarks = [20, 24, 3, 5, 10, 15, 0];
+var isVote = [true, false];
+console.log(isVote[1]);
+console.log(studentsMarks[3]);
+var fruits = ['Apple', 'Banana', 'orange'];
+fruits[1] = 'grapes';
+console.log(fruits);
